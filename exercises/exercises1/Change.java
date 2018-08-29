b86811c5a7e41c45eb1c43e26115b3e1f272986e
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-08-28 22:24:36 -0500

    exercise1

--- exercises/exercises1/Change.java
+++ exercises/exercises1/Change.java
@@ -8,14 +8,20 @@ public class Change {
 		//
 		// Below, prompt the user to enter a number of pennies
 		//
-		
-		
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int cents=ap.nextInt("Starting number of cents?");
 		//
 		// Then, compute and print out how many 
 		//    dollars, quarters, dimes, nickels, and pennies
 		// should be given in exchange for those pennies, so as to
 		// minimize the number of coins (see the videos)
 		//
+		int dollars=cents/100;
+		int quarters=(cents%100)/25;
+		int dimes=(cents%25)/10;
+		int nickels=((cents-25*quarters)%10)/5;
+		int pennies=cents%5;
+		System.out.println(dollars+" dollars "+quarters+" quarters "+dimes+" dimes "+nickels+" nickels "+pennies+"pennies ");
 	}
 
 }
