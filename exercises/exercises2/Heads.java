c926697bb4c6549f00c6fd16635bf2f3d50ca43f
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-09-05 22:31:21 -0500

    Yue Huang 100runs 10 heads problem

--- exercises/exercises2/Heads.java
+++ exercises/exercises2/Heads.java
@@ -7,11 +7,11 @@ public class Heads {
 	public static void main(String[] args) {
 
 		// what are the concepts?
+		for (int a = 0; a <100; ++a) {
 		int numHeads = 0;
 		int numFlips = 0;
-
 		// now flip a coin until we see 10 heads
-		while (numHeads != 10) {
+		while (numHeads !=10) {
 			boolean isHeads = Math.random() < 0.5;
 			if (isHeads) {
 				numHeads = numHeads + 1;
@@ -19,10 +19,11 @@ public class Heads {
 			numFlips = numFlips + 1;
 
 		}
+			System.out.println("The number of flips was " + numFlips);
+	}
+	}
 		// here, numHeads should be 10
 		
-		System.out.println("Number of flips was " + numFlips);
+	
 
 	}
-
-}
