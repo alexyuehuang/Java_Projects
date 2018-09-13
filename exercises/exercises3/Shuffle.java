0a52ae488940de8e9d20309b2decb0c2a5e7f1db
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-09-12 22:52:15 -0500

    Alex shuffle

--- exercises/exercises3/Shuffle.java
+++ exercises/exercises3/Shuffle.java
@@ -6,12 +6,12 @@ public class Shuffle {
 		String[] original = { "A", "B", "C", "D",
 				"E", "F", "G", "H"
 		};
-
+		String[] shuffle = new String [8];
 		// print out original array
 		for (int i=0; i < original.length; ++i) {
 			System.out.println("Original at " + i + " is " + original[i]);
 		}
-
+		int c = 0; 
 		//
 		// Follow the instructions on the web page to make a copy of
 		// the original array, named shuffled, but with its elements
@@ -19,6 +19,18 @@ public class Shuffle {
 		// shuffled array contains the same strings, but in a randomized
 		// order.
 		//
+		while (c< original.length) {
+			double k = ((Math.random())*10000)%8;
+			int q = (int)k;
+			if (original[q] !="") {
+				shuffle [c] = original [q];
+				original [q] = "";
+				c++;
+			}
+		}
+		for (int t=0; t < original.length; ++t) {
+			System.out.println("Shuffle at " + t + " is " + shuffle[t]);
+		}
 
 	}
 	
