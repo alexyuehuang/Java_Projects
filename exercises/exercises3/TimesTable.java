057c7d28ab66e05b7adfcf0002ac4085885cfce0
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-09-12 23:26:01 -0500

    Alex timestable

--- exercises/exercises3/TimesTable.java
+++ exercises/exercises3/TimesTable.java
@@ -9,8 +9,19 @@ public class TimesTable {
 		int N = ap.nextInt("Max value for table?");
 		
 		//  Table should include rows and columns for 0...N  
-		//     mkaing N+1 rows and columns
-		
+		//     making N+1 rows and columns
+		int[][] table = new int[N+1][N+1];
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j<= N; j++) {
+				table[i][j]= i * j; 
+			}
+		}
+		for (int s = 0; s <= N; s++) {
+			for (int t = 0; t<= N; t++) {
+				System.out.print(table[s][t]+ "  ");
+			}
+			System.out.println("");
+		}
 	}
 
 }
