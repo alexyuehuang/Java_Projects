7fb856ea9f4429541ad4885ca616247d785a1e12
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-09-12 22:24:37 -0500

    Alex copy array

--- exercises/exercises3/CopyArrayAndPigLatin.java
+++ exercises/exercises3/CopyArrayAndPigLatin.java
@@ -1,10 +1,20 @@
 package exercises3;
 
+import com.sun.org.apache.bcel.internal.generic.NEWARRAY;
+
 public class CopyArrayAndPigLatin {
 
 	public static void main(String[] args) {
 		String[] names = { "Alice", "Bob", "Carole", "David", "Elaine" };
+		String[] copy = new String[names.length];
+		for (int i = 0; i<names.length; i++) {
+			copy[i]= names[i]+ "ay";
+		}
+		for (int c = 0; c<names.length; c++) {
+			
 		
+		System.out.println(copy[c]);
+		}
 		// first copy the above array into an array named copy
 		//  Pretend the names array is large
 		//  So we cannot just do the following (too much to type):
