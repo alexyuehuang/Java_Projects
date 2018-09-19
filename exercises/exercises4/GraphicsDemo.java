bd1109182b43902f89e849fb4e87da687b8a552d
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-09-19 00:01:05 -0500

    alex graphics demo

--- exercises/exercises4/GraphicsDemo.java
+++ exercises/exercises4/GraphicsDemo.java
@@ -8,15 +8,24 @@ public class GraphicsDemo {
 
 	public static void main(String[] args) {
 		// blue point (look carefully, will be very small on your screen)
-		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.point(0.5, 0.5);
 		// larger green point
-		
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.circle(0.4,0.6, 0.2);
 		// unfilled red triangle
-		
+		StdDraw.setPenColor(Color.RED);
+		double[] a = {0.3,  0.2, 0.4};
+		double[] b = {0.1, 0.2, 0.1};
+		StdDraw.polygon(a, b);
 		// yellow circle, filled
-
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledCircle(0.1, 0.9, 0.1);
 		// filled blue rectangle
-		
+		StdDraw.setPenColor(Color.BLUE);
+		double[] c= {0.1, 0.3, 0.3, 0.1};
+		double[] d = {0.1, 0.1, 0.2, 0.2};
+		StdDraw.filledPolygon(c, d);
 
 
 	}
