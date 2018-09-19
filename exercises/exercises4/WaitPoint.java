35d0c2764446ed61d739ee6ed1b58db7aa348ef5
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-09-19 14:46:23 -0500

    Alex Waitpoint exercise

--- exercises/exercises4/WaitPoint.java
+++ exercises/exercises4/WaitPoint.java
@@ -6,20 +6,31 @@ public class WaitPoint {
 
 	public static void main(String[] args) {
 		// wait for the mouse to be pressed and released
-
+		while (!StdDraw.mousePressed()) {
+			StdDraw.pause(1000);
+		}
+		System.out.println("A");
 		// here, the mouse has been pressed
-
+		while (StdDraw.mousePressed()) {
+		StdDraw.pause(1000);
+		}
+		System.out.println("k");
 		// here the mouse is released
 		
 		
 		// draw a point at the location of the mouse
-		
-		
+		double x= StdDraw.mouseX();
+		double y= StdDraw.mouseY();
+		StdDraw.point(x, y);
 		// here, a q has been typed
-		
+		while (!StdDraw.hasNextKeyTyped()) {
+			StdDraw.pause(1000);
+		}
+		char key = StdDraw.nextKeyTyped();
+		if(key == 'q') {
 		
 		StdDraw.text(0.5, 0.5, "Farewell!");
-
+		}
 	}
 
 }
