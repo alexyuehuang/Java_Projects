7d015ad5fdda05e778d2cd98bccc67b6d8b042de
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-09-19 15:24:54 -0500

    Alex exercise pitches

--- exercises/exercises4/Pitches.java
+++ exercises/exercises4/Pitches.java
@@ -5,6 +5,12 @@ import cse131.ArgsProcessor;
 public class Pitches {
 
 	public static void main(String[] args) {
+		ArgsProcessor ap =new ArgsProcessor (args);
+		double k = ap.nextDouble("what is the pitch?");
+		double frequency = 440.0*Math.pow(2,(k/12));
+		double frequency7 = 440.0*(Math.pow(2, ((7+k)/12)));
+		double ratio  = frequency7/frequency;
+		System.out.println(frequency +"\n"+ frequency7+ "\n"+ ratio);
 		// encoding has 0 as concert A
 		//    1 would be Bb, just above A
 		//   -1 would be Ab, just below A
