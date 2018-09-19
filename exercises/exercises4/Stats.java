10453f357cf3409313c4eda5a54199f41830935c
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-09-18 23:44:12 -0500

    Alex stats exercise

--- exercises/exercises4/Stats.java
+++ exercises/exercises4/Stats.java
@@ -6,14 +6,31 @@ import cse131.ArgsProcessor;
 public class Stats {
 
 	public static void main(String[] args) {
-		// prompt the user for the file to be used for this run
+		// prompt the user for the file to be used for this run 
 		ArgsProcessor.useStdInput("datafiles/average");
-
+		
 		//
 		//  Read in the data from the opened file, computing the
 		//     sum, average, count, max, and min
 		//  of the data
 		//
+		double sum =0;
+		int count = 1;
+		double max = 0;
+		double min = StdIn.readDouble();
+		while (!StdIn.isEmpty()) {
+			double d = StdIn.readDouble();
+			count++;
+			sum = sum +d;
+			if (d>max) {
+				max =d;
+			}
+			if (d<min) {
+				min = d;
+			}
+		}
+		double average = sum/(double)count;
+		System.out.println(sum +"\n"+ count+"\n"+ average+ "\n"+ max +"\n"+ min);
 		//  count is the number of doubles read from the file
 		//  The other statistics should be clear from their names
 		//
