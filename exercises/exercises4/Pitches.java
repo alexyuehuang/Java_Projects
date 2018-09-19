b1581885a38921d052dfb836d0fc2313c9f19273
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-09-19 15:28:16 -0500

    Alex pitch ratio

--- exercises/exercises4/Pitches.java
+++ exercises/exercises4/Pitches.java
@@ -6,11 +6,12 @@ public class Pitches {
 
 	public static void main(String[] args) {
 		ArgsProcessor ap =new ArgsProcessor (args);
-		double k = ap.nextDouble("what is the pitch?");
+		double k = ap.nextDouble("what is pitch 1?");
+		double j = ap.nextDouble("what is pitch 2?");
 		double frequency = 440.0*Math.pow(2,(k/12));
-		double frequency7 = 440.0*(Math.pow(2, ((7+k)/12)));
-		double ratio  = frequency7/frequency;
-		System.out.println(frequency +"\n"+ frequency7+ "\n"+ ratio);
+		double frequency2 = 440.0*(Math.pow(2, (j/12)));
+		double ratio  = frequency/frequency2;
+		System.out.println(frequency +"\n"+ frequency2+ "\n"+ ratio);
 		// encoding has 0 as concert A
 		//    1 would be Bb, just above A
 		//   -1 would be Ab, just below A
