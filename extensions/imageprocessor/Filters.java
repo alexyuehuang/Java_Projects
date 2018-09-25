831f82a489990c772136fe274f4ed4b2695fd5d7
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-09-25 14:05:15 -0500

    Alex Huang filters

--- extensions/imageprocessor/Filters.java
+++ extensions/imageprocessor/Filters.java
@@ -44,54 +44,84 @@ public class Filters {
 	 *the first source image to the target panel.  
 	 *(Hint: This is a very simple method.)**/
 	public static int copy(int pixelComponent) {
-		return 0;  // FIXME
+		return pixelComponent;
 	}
 
 	//This method averages the color components of two pixels.
 	// USED IN: composite
 	public static int composite(int a, int b) {
-		return 0;  // FIXME
+		return (a +b)/2;
 	}
 
 	//This method returns the negative of a pixel by inverting its color components.
 	// USED IN: negative
 	public static int negative(int a) {
-		return 0;  // FIXME
+		return (256-a);
 	}
 
 	//This method reduces the number of possible values for a given color component
 	//from 256 to 2, by returning either 0 or 255 based on the original value.
 	// USED IN: posterize
 	public static int posterize(int a) {
-		return 0;   // FIXME
+		int b = a/128;
+		int c = (b==1)? 255:0;
+		return c;
 	}
 
 	//This method returns a color that is brighter than the original color.
 	// USED IN: brighter
 	//FIX ME
 	public static Color brighter(Color c) {
-		return Color.black;  // FIXME
+		return c.brighter();
 	}
 
 	//This method returns a color that is some shade of gray, by making a new
 	//color having equal RGB components. returns an array of integers [r, g ,b].
 	// USED IN: grayscale
 	public static Color grayscale(Color c) {
-		return Color.black;  // FIXME
+		int a = c.getRed();
+		int b = c.getBlue();
+		int d = c.getGreen();
+		int e = (a+b+d)/3;
+		Color q = new Color (e, e, e);
+		return q;  // FIXME
 	}
 
 	//This method returns either black or white, based on the intensity of the
 	//originally provided color. returns an array of integers [r, g ,b].
 	// USED IN: blackWhite
 	public static Color blackAndWhite(Color c) {
-		return Color.black;   // FIXME
+		int a = c.getRed();
+		int b = c.getBlue();
+		int d = c.getGreen();
+		Color q = new Color (0, 0, 0);
+		if(a + b + d < 256) {
+			q = Color.BLACK;
+		}
+		else {
+			q = Color.WHITE;
+		}
+		return q;   // FIXME
 	}
 
 	//This method combines two images by choosing for each location the brighter 
 	//pixel in the same location from the two source images.
 	// USED IN: combineBrighter
 	public static Color combineBrighter(Color c1, Color c2) {
-		return Color.black;  // FIXME
+		int a = c1.getRed();
+		int b = c1.getBlue();
+		int d = c1.getGreen();
+		int x = c2.getRed();
+		int y = c2.getBlue();
+		int z = c2.getGreen();
+		Color q = new Color (0,0,0);
+		if(a + b + d > x + y + z) {
+			q = c1;
+		}
+		else {
+			q = c2;
+		}
+		return q;  // FIXME
 
 
 	}
@@ -110,7 +140,20 @@ public class Filters {
 	 * @return
 	 */
 	public static Color bgSubtract(Color source1Color, Color source2Color, int tolerance) {
-		return Color.black;
+		Color q = new Color (0,0,0);
+		int a = source1Color.getRed();
+		int b = source1Color.getBlue();
+		int d = source1Color.getGreen();
+		int x = source2Color.getRed();
+		int y = source2Color.getBlue();
+		int z = source2Color.getGreen();
+		if (Math.abs(a-x)<= tolerance && Math.abs(b-y)<= tolerance && Math.abs(d-z)<= tolerance) {
+			q = Color.BLUE;
+		}
+		else {
+			q = source1Color;
+		}
+		return q;
 
 	}
 
@@ -122,7 +165,14 @@ public class Filters {
 	//second image if the color from the first image is blue; otherwise returns
 	//the color from the first image.
 	public static Color bgReplace(Color s1Color, Color s2Color) {
-		return genRandomColor();
+		int a = s1Color.getRed();
+		int b = s1Color.getBlue();
+		int d = s1Color.getGreen();
+		Color q = new Color (a, d, b);
+		if (a ==0 && b==255 && d==0) {
+			q = s2Color;
+		}
+		return q;
 	}
 
 }
