c81aa602ff613eeb25ef018bd625442b80208ba5
huangyue <huangyue@huangyuedembp.dhcp.wustl.edu>
2018-09-27 13:19:20 -0500

    Homeowning sorting Alex Huang extensions

--- extensions/arrays/Sorting.java
+++ extensions/arrays/Sorting.java
@@ -4,7 +4,53 @@ import cse131.ArgsProcessor;
 public class Sorting {
 	
 	public static void main(String[] args) {
-		
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int size  = ap.nextInt("size?");
+		while(size<0) {
+			System.out.println("oops, the size cannot be negative");
+			size  = ap.nextInt("size?");
+		}
+		int[] array = new int[size];
+		for (int i= 0; i<size; i++) {
+			array[i] = ap.nextInt("next integer?");
+		}
+		for(int count = 1; count<size; count++) {
+			int min = array[count-1];
+			int n= min;
+			int m = count-1;
+			for (int j = count; j<size; j++) {
+				if (array[j]<min) {
+					min = array[j];
+					m=j;
+				}
+			}
+			array[count-1] = array[m];
+			array [m] = n;
+		}
+		for (int i= 0; i<size; i++) {
+			System.out.print(array[i]+" ");
+		}
+		double mean = 0.0;
+		for(int i= 0; i<size; i++) {
+			mean = mean +(double)array[i];
+		}
+		mean = mean/((double)size);
+		System.out.println("mean " +mean);
+		double median = 0;
+		if(size%2 ==0) {
+			median = ((double)(array[size/2]+array[size/2-1]))/2.0;
+		}
+		else {
+			median = array[(size-1)/2];
+		}
+		System.out.println("median " +median);
+		int min = array[0];
+		System.out.println("min " +min);
+		int max = array[size-1];
+		System.out.println("max " +max);
+		int range = max-min;
+		System.out.println("range " +range);
+
 	}
 
 }
--- extensions/homeowning/HomeOwning.java
+++ extensions/homeowning/HomeOwning.java
@@ -1,10 +1,29 @@
 package homeowning;
 
+import cse131.ArgsProcessor;
+
 public class HomeOwning {
 
 	public static void main(String[] args) {
-	
-
+	ArgsProcessor ap = new ArgsProcessor(args);
+	String name = ap.nextString("name of the house?");
+	String zip = ap.nextString("zip code of the house?");
+	int rent = ap.nextInt("monthly rent of the house?");
+	double m = ap.nextDouble("daily interest of the house?");
+	int rentY = rent*12;
+	double rentW = ((double)rent)* (7.0/(365.0/12.0));
+	rentW = (Math.round(rentW*100.0))/100.0;
+	double mY = m*365;
+	mY = (Math.round(mY*1000.0))/1000.0;
+	double mW = m*7;
+	mW = (Math.round(mW*1000))/1000.0;
+	System.out.println(name + " is located in the Zip Code " + zip + "\n" + "Rent per year: " + rentY + "\n" + "Rent per week: " + rentW + "\n" + "\n" + "Interest paid per year: " + mY + "\n" + "Interest paid per week: " + mW + "\n");
+	if (rentY >= mY) {
+		System.out.println("I should buy");
+	}
+	else {
+		System.out.println("I should rent");
+	}
 	}
 
 }
