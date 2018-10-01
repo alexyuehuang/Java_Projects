4cc87964a2dad9d5436e7d4ec1b7ff8bbc44b2ba
Dennis Cosgrove <cosgroved@wustl.edu>
2018-09-30 23:02:24 -0500

    equals over == tests

--- labs/lab5/tests/BinarySearchTestSuite.java
+++ labs/lab5/tests/BinarySearchTestSuite.java
@@ -3,13 +3,14 @@ package lab5.tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import lab5.tests.binarysearch.BinarySearchTest;
+import lab5.tests.binarysearch.BinarySearchComprehensiveTest;
+import lab5.tests.binarysearch.BinarySearchEqualsTest;
 import lab5.tests.binarysearch.MidpointTest;
 
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ MidpointTest.class, BinarySearchTest.class })
+@Suite.SuiteClasses({ MidpointTest.class, BinarySearchEqualsTest.class, BinarySearchComprehensiveTest.class })
 public class BinarySearchTestSuite {
 }
